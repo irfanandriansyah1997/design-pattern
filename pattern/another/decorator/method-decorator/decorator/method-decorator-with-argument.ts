43f24ee1e6986890c085a657dbd2a347f539615d
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-07-31 20:50:54 +0700

    feat(decorator): create example for method decorator

---
+++ pattern/another/decorator/method-decorator/decorator/method-decorator-with-argument.ts
@@ -0,0 +1,24 @@
+import LoggerBuilder from '../builder/logger.builder';
+
+/**
+ * Logger Decorator
+ * @author Irfan Andriansyah <irfan@99.co>
+ * @param {'error' | 'debug' | 'info' | 'log'} type - logger
+ * @since 2020.07.31
+ */
+export const loggerWithArgument = (type: 'error' | 'debug' | 'info' | 'log') => (
+    _: object,
+    propertyName: string,
+    propertyDesciptor: PropertyDescriptor
+): PropertyDescriptor => {
+    const method = propertyDesciptor.value;
+
+    propertyDesciptor.value = function (...args: any[]) {
+        const date = new Date().toUTCString();
+        const message = `[${date}]: DEBUG - call method ${propertyName}`;
+
+        new LoggerBuilder().setMessage(message).setType(type).execute();
+        return method.apply(this, args);
+    };
+    return propertyDesciptor;
+};
